d7ff2c5573fe43873cfcf119d5e02ddd7c9bc1d7
wim glenn <wim.glenn@gmail.com>
2018-12-27 16:51:48 -0600

    vbump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="johnnydep",
-    version="0.6",
+    version="0.7",
     description="Display dependency tree of Python distribution",
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
